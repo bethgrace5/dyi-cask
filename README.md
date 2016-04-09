93153e38dfda95cc977c6332cb915a080ed8620e
Beth Armitage <bethgrace5@gmail.com>
2016-04-09 12:30:17 -0700

    basic facebook dependency setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dyi-cask
+
+### Views
+* dashboard
+* event list
+* location list
+* user registration
+* safety tip banner
+* suggest event
+* view event
+* view location
+
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,36 @@
  *
  * Main module of the application.
  */
-angular
-  .module('dyiCaskApp', [
+angular.module('dyiCaskApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    'ngTouch',
+    'facebook'
+]).config(['$routeProvider', function($routeProvider) {
+  // Routes
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl',
+      controllerAs: 'about'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}])
+.config(['$facebookProvider', function($facebookProvider) {
+  // Facebook setup
+  $facebookProvider.init({
+    appId: '584673925032739',
+    channel: 'views/channel.html'
   });
+}]);
+
