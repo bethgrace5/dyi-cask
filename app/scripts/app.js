cc7b2cd85ea4d95e0c7e4578819bf415b1e3ab11
Beth Armitage <bethgrace5@gmail.com>
2016-04-09 10:24:52 -0700

    initial yeoman angular

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dyiCaskApp
+ * @description
+ * # dyiCaskApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dyiCaskApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dyiCaskApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dyiCaskApp
+ */
+angular.module('dyiCaskApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
