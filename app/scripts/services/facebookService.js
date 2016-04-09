ae8bfe510b3fae3f24e82b76d528b4a3b774d917
Jacob Sherman <jakesher@jMacPro.local>
2016-04-09 15:25:26 -0700

    ...

---
+++ app/scripts/services/facebookService.js
@@ -0,0 +1,19 @@
+angular.module('dyiCaskApp')
+
+.factory('facebookService', function($q, FB) {
+    return {
+        getMyLastName: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {
+                fields: 'last_name'
+            }, function(response) {
+                if (!response || response.error) {
+                    deferred.reject('Error occured');
+                } else {
+                    deferred.resolve(response);
+                }
+            });
+            return deferred.promise;
+        }
+    }
+})
