f722dd964234d46dc5de37c6c318ed118586414c
Austin Chavez <austchavez@gmail.com>
2016-04-09 15:58:02 -0700

    Added some page_ids in main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,6 +13,33 @@ angular.module('dyiCaskApp')
     $log,
     $scope) {
 
+    $scope.locations = [
+      {"name": "",
+      "id": "410616265786425",
+      "events": []},
+      {"name": "",
+      "id": "875835105860557",
+      "events": []},
+      {"name": "",
+      "id": "941260815906419",
+      "events": []},
+      {"name": "",
+      "id": "243499665673625",
+      "events": []},
+      {"name": "",
+      "id": "356550797717326",
+      "events": []},
+      {"name": "",
+      "id": "750619864976173",
+      "events": []},
+      {"name": "",
+      "id": "96361922275",
+      "events": []},
+      {"name": "",
+      "id": "302943546438648",
+      "events": []}
+      ];
+
     // bind directly to the response promise
     $scope.loginStatus = $facebook.getLoginStatus();
     //$log.debug($scope.loginStatus);
@@ -27,6 +54,8 @@ angular.module('dyiCaskApp')
         $scope.loginError = response.error;
         $log.debug(response);
       });
+
+    //$scope.locations
 
 
   });
