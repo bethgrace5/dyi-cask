f120f1dffed0dda90b6ae3676b215cb0199d5df4
Jacob Sherman <jakesher@gmail.com>
2016-04-10 07:28:28 -0700

    Aggregated location event data, filtered for current events, and sorted ascending. (test)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,45 +8,94 @@
  * Controller of the dyiCaskApp
  */
 angular.module('dyiCaskApp')
-  .controller('MainCtrl', function (facebookService, $log, $scope) {
+  .controller('MainCtrl', function (facebookService, $q, $log, $scope) {
 
-    $scope.locations = [
-      {"name": "",
-      "id": "410616265786425",
-      "events": []},
-      {"name": "",
-      "id": "875835105860557",
-      "events": []},
-      {"name": "",
-      "id": "941260815906419",
-      "events": []},
-      {"name": "",
-      "id": "243499665673625",
-      "events": []},
-      {"name": "",
-      "id": "356550797717326",
-      "events": []},
-      {"name": "",
-      "id": "750619864976173",
-      "events": []},
-      {"name": "",
-      "id": "96361922275",
-      "events": []},
-      {"name": "",
-      "id": "302943546438648",
-      "events": []}
+    $scope.locationRef = [
+      '410616265786425',
+      '875835105860557',
+      '941260815906419',
+      '243499665673625',
+      '356550797717326',
+      '750619864976173',
+      '96361922275',
+      '302943546438648',
+      '290894474287798'
       ];
+
+    $scope.getPagesEventsById = function(page_ids, after_dttm)
+    {
+      var promises = [];
+
+      for (var i = 0; i < page_ids.length; i++)
+        promises.push(facebookService.getPageEventsById(page_ids[i]));
+
+      $q.all(promises).then(
+        function(response) {
+          var locations = [];
+
+          for (var j = 0; j < response.length; j++)
+          {
+            var events = response[j].data
+                              .map(function (e) { return {
+                                place_name: e.place.name,
+                                event_name: e.name,
+                                location: e.place.location,
+                                start_time: new Date(e.start_time) }; 
+                              })
+                              .filter(function (e) { return e.start_time.getTime() >= after_dttm.getTime(); })
+                              .sort(function (l, r) { 
+                                      if (l.start_time.getTime() < r.start_time.getTime()) { return -1; }
+                                      else if (l.start_time.getTime() > r.start_time.getTime()) { return 1; }
+
+                                      return 0; 
+                              });
+
+            
+            if (events != null && events.length > 0)
+              locations.push({ place_name: events[0].place_name, 
+                                             events: events });
+          }
+
+          $scope.locations = locations;
+          $log.debug($scope.locations);
+        },
+        function(response) {
+          $scope.locations = [];
+        });
+    }  
+
+    $scope.getPageEventsById = function(page_id, after_dttm)  
+    {
+      facebookService.getPageEventsById(page_id).then(
+        function(response) {
+          var events = response.data;
+
+          $scope.currentEvents = events
+                                  .map(function (e) { return {
+                                    place_name: e.place.name,
+                                    event_name: e.name,
+                                    location: e.place.location,
+                                    start_time: new Date(e.start_time) }; 
+                                  })
+                                  .filter(function (e) { return e.start_time.getTime() >= after_dttm.getTime(); })
+                                  .sort(function (l, r) { 
+                                      if (l.start_time.getTime() < r.start_time.getTime()) { return -1; }
+                                      else if (l.start_time.getTime() > r.start_time.getTime()) { return 1; }
+
+                                      return 0; 
+                                  });
+
+          $log.debug($scope.currentEvents);
+        },
+        function(response) { 
+          $scope.currentEvents = [];
+        });
+    }
 
     $scope.$on('fb-init', function(event, args) 
     {
-      facebookService.getPageEventsById(108905739150385).then(
-      function(response) 
-      {
-        $log.debug(JSON.stringify(response));
-      },
-      function(response) { 
-        $log.debug(JSON.stringify(response));
-      });
+      //$scope.getPageEventsById('941260815906419', new Date());
+      $scope.getPagesEventsById($scope.locationRef, new Date());
     });
     
 
